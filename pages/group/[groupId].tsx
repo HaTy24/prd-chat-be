a10c469214effd1b5a72831883eccec3a11011ce
Ty Ha <tyha@dayoneteams.com>
2023-09-26 01:45:23 +0700

    feat: sent message

--- pages/group/[groupId].tsx
+++ pages/group/[groupId].tsx
@@ -1,42 +1,38 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-import { Group } from "../../packages/group/models/group";
-import { groupService } from "../../packages/group/services/groupService";
-import { Socket } from "socket.io-client";
+import io from "socket.io-client";
+let socket;
 
 export default function GroupPage() {
   const [message, setMessage] = useState<string>();
-  const [allMessage, setAllMessage] = useState({});
+  const [allMessage, setAllMessage] = useState([]);
   const router = useRouter();
+  const msgs = [];
   const { groupId } = router.query;
+
+  const socketInitializer = async () => {
+    socket = io("http://localhost:4000");
+
+    socket.emit("joinGroup", { groupId });
+
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+
+    socket.on("messageCreated", (msg) => {
+      msgs.push(msg);
+      setAllMessage([...msgs]);
+    });
+  };
+
   useEffect(() => {
-    if (router.isReady) {
-      (async () => {
-        io("http://localhost:4000").emit("joinGroup", groupId);
-        const data = await groupService.getAllMessage(groupId as string);
-        setAllMessage(data);
-      })();
-    }
-    console.log("reruen");
-  }, [router.isReady]);
-
-  io("http://localhost:4000").on("messageCreated", (response) => {
-    const clone = allMessage;
-    clone[groupId as string]
-      ? clone[groupId as string].push(response)
-      : (clone[groupId as string] = [response]);
-    setAllMessage(clone);
-  });
-
-  io("http://localhost:4000").on("haha", (response) => {
-    console.log(response);
-  });
+    socketInitializer();
+  }, []);
 
   const sendMessage = async (e) => {
     e.preventDefault();
-    groupService.sendMessage({ groupId: groupId as string, message });
+    socket.emit("message", { groupId, message });
     setMessage("");
   };
 
@@ -48,7 +44,7 @@ export default function GroupPage() {
       </Head>
       <div className="container mt-5">
         <ul>
-          {allMessage[groupId as string]?.map((mess, i) => {
+          {allMessage.map((mess, i) => {
             return (
               <li key={i}>
                 <span className="text-primary">{mess.clientId}: </span>
